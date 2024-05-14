31b6d3bf0b98f64b04f0a4fcf85bec6d3a1f0824
damianrein <damianrein464@gmail.com>
2024-05-14 13:58:43 -0300

    1.0.0

---
+++ src/main/java/com/ApiBatch/ApiBatchApplication.java
@@ -0,0 +1,13 @@
+package com.ApiBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApiBatch/ApiBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
